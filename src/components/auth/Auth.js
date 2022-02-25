9a4f1f4f0f6e26e1e1d918dbcf38fcfacfc9a883
tifeolanipekun <tifeolanipekun@gmail.com>
2022-02-25 11:48:25 +0100

    Frontend task from my end

---
+++ src/components/auth/Auth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react'
+import Register from "./Register";
+
+const Auth = () => {
+    return (
+        <Box bg="#7FD5FA" width="100%" height="100vh">
+        <Register/>
+        </Box>
+    );
+};
+
+export default Auth;
